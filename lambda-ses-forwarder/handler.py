45b2c7179f32d0dc638d7ecec498a5f99516c058
daniel millikan <danielmillikan@pro.local>
2019-09-12 15:52:23 -0500

    final commit

--- lambda-ses-forwarder/handler.py
+++ lambda-ses-forwarder/handler.py
@@ -1,24 +1,48 @@
 import json
 import email
+import boto3
+import os
 
 def process_event(event, context):
- 
+    """Forward the mail"""
+    ses_client = boto3.client('ses')
+    emailAddress = os.environ['RECEPIENT_EMAIL']
     for record in event['Records']:
+        # print(record['Sns']['Message'])
         msgJson = json.loads(record['Sns']['Message'])
-        msgSubject = msgJson['mail']['commonHeaders']['subject']
+        subject = 'FW:' + msgJson['mail']['commonHeaders']['subject']
+        # print(msgJson)
 
+        # print("Mail Subject IS : {0}".format(subject))
+        
         msgContent = msgJson['content']
 
-        msg = email.message_from_string(msgContent)
+        for msg in email.message_from_string(msgContent).get_payload():
+            if msg.get_content_type() == "text/plain":
+                st = str(msg.get_payload())
+            elif msg.get_content_type() == "text/html":
+                html = str(msg.get_payload())
+        
 
-        st = ''
-        if msg.get_content_type() == "text/plain":
-                    st = str(msg.get_payload())
-        else:    
-            for payload in msg.get_payload():
-                if payload.get_content_type() == "text/plain":
-                    st = str(payload.get_payload())
-
-        print(st)
-
+        response = ses_client.send_email(
+            Source=emailAddress,
+            Destination={'ToAddresses': [emailAddress]},
+            Message={
+                'Subject': {
+                    'Data': subject,
+                    'Charset': 'UTF-8'
+                },
+                'Body': {
+                    'Html': {
+                        'Charset': 'UTF-8',
+                        'Data': html
+                    },
+                    'Text': {
+                        'Data': st,
+                        'Charset': 'UTF-8'
+                    }
+                }
+            }
+        )
+        print(response)
     return
